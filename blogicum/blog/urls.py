2eee9c511acd20d37bed32c0770e433a134a86d9
Тимофей Разборщиков <ash_tray85@mail.ru>
2023-07-12 23:06:49 +0300

    Change main.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-     path('', views.PostListView.as_view(), name='index'),
+     path('', views.PostListView.as_view(),
+          name='index'),
      path('posts/<int:pk>/', views.PostDetailView.as_view(),
           name='post_detail'),
      path('category/<slug:category_slug>/',
