29844a4b5c6392ad90a14336e823c9007e165825
Тимофей Разборщиков <ash_tray85@mail.ru>
2023-07-12 22:44:14 +0300

    Change main.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -18,11 +18,9 @@ urlpatterns = [
          name='delete_post'),
     path('posts/<int:pk>/comment/', views.add_comment, name='add_comment'),
     path('posts/<int:post_id>/edit_comment/<int:comment_id>/',
-         views.edit_comment, name='edit_comment'
-         ),
+          views.edit_comment, name='edit_comment'),
     path('posts/<int:post_id>/delete_comment/<int:comment_id>',
-         views.delete_comment, name='delete_comment'
-         ),
+          views.delete_comment, name='delete_comment'),
     path('profile/<str:name>/', views.info_profile, name='profile'),
     path('edit_profile/<slug:name>', views.edit_profile,
          name='edit_profile'),
